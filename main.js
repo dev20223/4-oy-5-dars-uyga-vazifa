0c9bb01d84ff411038c43ce54d12562c254b49d7
dev20223 <chingizbekturdiboyev@gmail.com>
2023-12-16 21:51:44 +0500

    Uyga vazifa

---
+++ main.js
@@ -0,0 +1,53 @@
+// 1-misol
+
+// let a = +prompt("Birinchi sonni kiriting:");
+// let b = +prompt("Ikkinchi sonni kiriting:");;
+// let result = 0;
+
+// for (let i = 0; i < b; i++) {
+//   result += a;
+// }
+
+// console.log(result); 
+
+// 2-masala
+
+// function countDigits(number) {
+//     const strNumber = String(number);
+//     let count = 0;
+    
+//     for (let i = 0; i < strNumber.length; i++) {
+//       if (!isNaN(Number(strNumber[i]))) {
+//         count++;
+//       }
+//     }
+    
+//     return count;
+//   }
+
+// 3-masala
+
+// function factorial(n) {
+//     if (n < 0) {
+//       throw new Error("Faktorial son emas");
+//     }
+    
+//     if (n === 0 || n === 1) {
+//       return 1;
+//     }
+    
+//     return n * factorial(n-1);
+//   }
+
+// 4-masala
+
+// let password = prompt("Parolni Kiriting:");
+// while (true) {
+// const userInput = prompt("Parolni Kiriting:");
+// if (userInput === null) {
+// break;
+// }
+// password = userInput;
+// }
+
+
